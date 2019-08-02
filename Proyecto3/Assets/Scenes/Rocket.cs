3103ebad732dd90f8b6ac5b27a14d89995afe9ad
Israel de Lamo Blas <i.delamo@gmail.com>
2019-08-02 22:36:04 +0100

    Añadido el keydown para espacio, A y D

---
+++ Proyecto3/Assets/Scenes/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    
+    private void ProcessInput()
+    {
+        /*
+         Puedo empujar siempre
+         */
+
+        if (Input.GetKey(KeyCode.Space)) {
+            print("espaciejo");
+        }
+
+        /*
+         Puede rotar pero solo hacia un lado u otro
+         */
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotacion A");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("rotacion D");
+        }
+    }
+}
