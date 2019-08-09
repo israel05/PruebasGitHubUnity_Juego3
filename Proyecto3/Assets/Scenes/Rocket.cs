640d3d8be4d8fe62910c9101a0f9d7948e13f736
Israel de Lamo Blas <i.delamo@gmail.com>
2019-08-09 16:04:57 +0100

    Cohete con sonido

--- Proyecto3/Assets/Scenes/Rocket.cs
+++ Proyecto3/Assets/Scenes/Rocket.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody; //para el cohete
+    AudioSource audioSource; //para el sonido del misisl
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+  
         
     }
 
@@ -24,8 +31,18 @@ public class Rocket : MonoBehaviour
          Puedo empujar siempre
          */
 
-        if (Input.GetKey(KeyCode.Space)) {
-            print("espaciejo");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+          //  print("espaciejo");
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         /*
@@ -33,11 +50,13 @@ public class Rocket : MonoBehaviour
          */
         if (Input.GetKey(KeyCode.A))
         {
+            transform.Rotate(Vector3.forward);
             print("rotacion A");
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("rotacion D");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
